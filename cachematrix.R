2a015e907231277672759bfb0a61155636197991
Daniel Payne <dwpandq@gmail.com>
2020-10-01 10:13:55 -0400

    Update cachematrix.R

--- cachematrix.R
+++ cachematrix.R
@@ -13,23 +13,3 @@ makeCacheMatrix <- function(x = matrix()) {
 cacheSolve <- function(x, ...) {
         ## Return a matrix that is the inverse of 'x'
 }
-<<<<<<< HEAD
-
-
-#TEST GIT and again please
-
-#Where will this go now?>
-
-#what is going on
-#I have no idea this is just plain weird
-#why isn't it doing any damn thing.
-print("x")
-"this is a test"
-
-
-fdasfdafdasfdasfdafdas
-(run away now
-    )
-
-=======
->>>>>>> bc242fed3b55d4e24cedf128b344b2efd60b633c
